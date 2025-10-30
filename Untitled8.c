32517ec0387071989566a6e81c504b6fcb92493a
Rishi-AUS <rishi213643@gmail.com>
2025-10-30 11:59:04 +0530

    Add files via upload

---
+++ Untitled8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+	int l,b,area;
+	printf ("enter l value");
+	scanf ("%D",&l);
+	printf ("enter b value");
+	scanf ("%d",&b);
+	area=l*b;
+	printf("area of rectangle=%d",area);
+	return 0;
+}
