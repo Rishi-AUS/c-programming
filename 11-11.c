ad1b61c49a95b0d175a1394d75c58506f66c9b57
Rishi-AUS <rishi213643@gmail.com>
2025-11-18 15:57:10 +0530

    Add files via upload

---
+++ 11-11.c
@@ -0,0 +1,16 @@
+int main()
+{
+n	int a[10][10],b[10][10],c[10][10],r,c,i,j;
+	printf ("enter matrix rows and column size");
+	scanf ("%d%d",&r,&c);
+	printf("enter A matrix values");
+	for(i=0;i<r;i++)
+	{
+		for(i=0;i<r;i++)
+		{
+			for(j=0;j<<;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+
