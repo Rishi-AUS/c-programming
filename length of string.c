356419ad8e1d73293275d2b49d8555a10b52f765
Rishi-AUS <rishi213643@gmail.com>
2025-11-25 14:42:55 +0530

    Add files via upload

---
+++ length of string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	char s [20];
+	int l;
+	printf ("enter string name");
+	gets (s);
+	for ("i=0;s[i]!="\0",i++");
+	l=i;
+	printf ("length of the string=%d"l);
+	return 0;
+}
