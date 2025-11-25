0b1fb50ba5103e47b183fd30e01a618cdb2d7fe7
Rishi-AUS <rishi213643@gmail.com>
2025-11-25 14:43:19 +0530

    Add files via upload

---
+++ without strcpy.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char S1[20],S2[20];
+	printf("enter string name:");
+	scanf("%s",&S1);
+	strcpy(S2,S1);
+	printf("original string=%s",S1);
+	printf("\ncopied string=%s",S2);
+	return 0;
+}
